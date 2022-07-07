ff386290022fb6ab16e034e10bd3f6f7f985d653
GhosTHaise <Ghostrex2@gmail.com>
2022-07-07 21:51:18 +0300

    Use style-components

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,16 @@ import { useWeb3 } from '@3rdweb/hooks'
 export default function Home() {
   const {address ,connectWallet} = useWeb3();
   return (
-    <div>GhosT
+    <Wrapper>
         <button onClick={() => {connectWallet('injected')}}>Connect Wallet</button>
-    </div>
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+    height:100vh;
+    max-width:100vw;
+    background-color:#0a0b0d;
+    color:white;
+    display:grid;
+    place-items:center;
+`
