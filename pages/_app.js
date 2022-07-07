5ae95ed6dbeaff192aaafafd09b8a4bb90a7f0d9
GhosTHaise <Ghostrex2@gmail.com>
2022-07-07 21:24:12 +0300

    Initialize project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from "@3rdweb/hooks"
+const supportedChainId = [4];
+const connectors = {
+  injected:{}
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider 
+        supportedChainIds={supportedChainId}
+        connectors={connectors}
+    >
+         <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
