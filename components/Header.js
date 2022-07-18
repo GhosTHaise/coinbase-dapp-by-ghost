1cfcced10caa549d871c62e13b160c1310719689
GhosTHaise <Ghostrex2@gmail.com>
2022-07-18 19:00:34 +0300

    Add modal and rooter

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import styled from "styled-components"
+/* installation necessaire from npm */
+import {usrRooter} from 'next-rooter'
+import TransfertModal from './Modal/TransfertModal'
+import Modal from 'react-modal'
+
 const Header = ({walletAddress}) => {
   return (
     <Wrapper>
---
+++ components/Modal/TransfertModal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TransfertModal = () => {
+  return (
+    <Wrapper> 
+        <p>Send</p>
+        <p>Receive</p>
+    </Wrapper>
+  )
+}
+const Wrapper = div`
+    color:black
+`;
+
+export default TransfertModal
