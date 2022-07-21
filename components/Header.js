773cbcfcde54797320d988017655850059019aac
GhosTHaise <Ghostrex2@gmail.com>
2022-07-21 19:27:50 +0300

    Add selected modal button

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,8 @@ Modal.setAppElement("#__next")
 
 const Header = ({walletAddress}) => {
   const router = useRouter();
-    //2:26:50
+  
+
   const customStyled = {
       content : {
           top: "50%",
--- components/Modal/TransfertModal.js
+++ components/Modal/TransfertModal.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
 
 const TransfertModal = () => {
+  const [action,setAction] = useState('send');
+    //2:26:50
+   const selectedStyle = {
+        color: '#3773f5'
+   }
+
+   const unselectedStyle = {
+        border : "1px solid #282b2f "
+   }
   return (
     <Wrapper> 
         <Selector>
-          <p>Send</p>
-          <p>Receive</p>
+          <Option style={action == "send" ? selectedStyle : unselectedStyle} onClick={() => setAction("send")}>
+              <p>Send</p>
+          </Option>
+          <Option style={action == "receive" ? selectedStyle : unselectedStyle} onClick={()=> setAction("receive")}> 
+              <p>Receive</p>
+          </Option>
         </Selector>
     </Wrapper>
   )
@@ -25,4 +38,18 @@ const Selector = styled.div`
     align-items:center;
     height:5rem;
 `;
+const Option = styled.div`
+    height:100%;
+    width:100%;
+    display:grid;
+    place-items:center;
+    font-size:1.2rem;
+    font-weight:600;
+    transition: all .3s;
+    &:hover{
+      cursor:pointer;
+      background-color:#111214;
+    }
+`;
+
 export default TransfertModal
